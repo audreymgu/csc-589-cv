ff9e6d1b9c41e8ad5ebb49a8b2cb5816b83f4c18
George Gu <george.zp.gu@gmail.com>
2017-09-18 21:51:26 -0400

    Completed and updated all sections.

--- pset-1/ProblemSetOne.py
+++ pset-1/ProblemSetOne.py
@@ -1,4 +1,6 @@
+import glob
 import cv2
+from skimage import io, img_as_float
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -17,42 +19,42 @@ c = np.array([0,2,3,2])
 
 # Find the dot product (also known as the inner product) of vectors a and b. Save the resulting value as variable 'aDotb'.
 aDotb = np.inner(a,b)
-print aDotb
+print 'aDotb' + aDotb
 
 # Find the element-wise product of a and b.
 a.shape = (3,1) # Convert to column
 b.shape = (3,1) # Convert to column
 aElementb = np.multiply(a,b)
-print aElementb
+print 'aElementb' + aElementb
 
 # Find (a^Tb)^Ma.
 aMatrixb = np.outer(b,a)
-print aMatrixb
+print 'aMatrixb' + aMatrixb
 
 # Without using a loop, multiply each row and column of M element-wise by a.
 a.shape = (1,3) # Convert to row
 M = np.multiply(M,a)
-print M
+print 'M element-wise by a' + M
 
 # Without using a loop, sort all of the values of the new M from (e) in increasing order and plot them in your report.
 M = np.sort(M, axis=None)
 M.shape = (4,3)
-print M
+print 'M sorted' + M
 
 # Section 3
 # Read in the images 'image1.jpg' and 'image2.jpg'.
 img1 = cv2.imread('image1.jpg',-1)
 img2 = cv2.imread('image2.jpg',-1)
 
-# Convert the images to double precision and rescale them to stretch from a minimum value of 0 to maximum value of 1.
+# Convert the images to double precision and rescale them to stretch from a minimum value of 0 to a maximum value of 1.
 img1 = np.float32(img1)
 img2 = np.float32(img2)
-print img1.dtype
-print img2.dtype
+# print img1.dtype
+# print img2.dtype
 img1 *= (1.0/img1.max())
 img2 *= (1.0/img2.max())
-print img1.max()
-print img2.max()
+# print img1.max()
+# print img2.max()
 
 # Add the images together and re-normalize them to have minimum value of 0 and maximum value of 1.
 img3 = np.add(img1, img2)
@@ -69,6 +71,39 @@ img4 = np.concatenate((img1[:,:(np.size(img1,1)/2)], img2[:,(np.size(img2,1)/2):
 img4 *= (255.0/img4.max())
 cv2.imwrite('image4.jpg', img4)
 
+# Using a for loop, create a new image such that every odd numbered row is the corresponding row from image1 and the every even row is the corresponding row from image2.
+img5 = np.copy(img1)
+for i in range(1, np.size(img5,0), 2):
+  img5[i:i+1] = img2[i:i+1]
+  
+# Normalize merged result for image display
+img5 *= (255.0/img5.max())
+cv2.imwrite('image5.jpg', img5)
+
+# Accomplish the same task without using a for loop.
+img6 = np.copy(img1)
+img6[1::2] = img2[1::2]
+
+# Normalize merged result for image display
+img6 *= (255.0/img6.max())
+cv2.imwrite('image6.jpg', img6)
+
+# Convert the result to a grayscale image.
+img7 = cv2.imread('image6.jpg',0)
+cv2.imwrite('image7.jpg', img7)
+
+# Section 4
+# Compute the average face of an individual, given a set of 100 or more images of said person.
+avg = np.zeros((250,250,3), dtype='float64')
+for filename in glob.glob('lfw/*.jpg'):
+  print filename
+  selection = io.imread(filename)
+  selection = np.array(selection, dtype='float64')
+  avg = avg + selection / 530
+  
+# Convert to uint8 and save image
+avg = np.array(avg, dtype='uint8')
+io.imsave('image8.jpg', avg)
 
 
 
